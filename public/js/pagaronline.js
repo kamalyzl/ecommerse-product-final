f59d9885aa45f983dfad5f21f1a11b29b8f6d6d1
JackelineGCP <gessenia_17@hotmail.com>
2018-02-25 11:59:16 -0500

    Agregando funcionalidad de la pasarela de pagos Culqi

--- public/js/pagaronline.js
+++ public/js/pagaronline.js
@@ -1,47 +1,63 @@
-// Configurando el checkout 
+// Seteando valores de config
 
-Culqi.publicKey = 'sk_test_v2XDSHbFpfjBRA5q';
-Culqi.settings({
-  title: 'Culqi Store',
-  currency: 'PEN',
-  description: 'Polo/remera Culqi lover',
-  amount: 3500
-  });
-
+Culqi.publicKey = 'pk_test_FP96YFVHPBdhfKqt'; // Colocar tu Código de Comercio (llave pública)
 Culqi.init();
 
-//Aquí creamos el Token en Culqi con la información de la tarjeta
-//Culqi.createToken();
-
+Culqi.settings({
+  title: 'Culqi Store',
+  currency: 'PEN', // Código de la moneda, 'PEN' o 'USD'
+  description: 'Polo/remera Culqi lover', // Descripción acerca de la compra
+  amount: 3500 // Monto de la compra (sin punto decimal, en este caso 35.00 soles)
+});
+$('#buyButton').on('click', function (e) {
+  // Abre el formulario con las opciones de Culqi.settings
+  Culqi.open();
+  e.preventDefault();
+});
+// Recibimos el token desde los servidores de Culqi
 function culqi() {
-  if(Culqi.token) { // ¡Token creado exitosamente!
-      // Obtener el token ID:
-      var token = Culqi.token.id;
-      $.ajax({
-        url: '',
-        data: {
-          token: token,
-          monto: 200 
-        },
-        type: 'JSON',
-        success: function(data){
-          console.log(data);
-        },
-        error: function(error_data){
-          console.log(error_data);
-        },
-      })
-  }else{ // ¡Hubo algún problema!
-      // Mostramos JSON de objeto error en consola
-      console.log(Culqi.error);
-      alert(Culqi.error.user_mensaje);
+  if (Culqi.token) { // Token creado exitosamente!
+    // Obtener el token ID
+    var token = Culqi.token.id;
+    alert('Se ha creado un token: ' + token);
+  } else { // Hubo algun problema!
+    // Mostramos JSON de objeto error en consola
+    console.log(Culqi.error);
+    alert(Culqi.error.mensaje);
   }
 };
 
-$(document).ready(function(){
-  $('#culqi-card-form').on('submit', function(e){
-    e.preventDefault();
-    Culqi.createToken();
-  });
-})
+/* 
 
+// Configura el formulario
+Culqi.publicKey = 'pk_test_FP96YFVHPBdhfKqt';
+Culqi.init();
+Culqi.createToken();
+
+Culqi.settings({
+  title: 'Culqi Store',
+  currency: 'PEN',
+  description: 'Polo Culqi lover',
+  amount: 3500
+});
+
+// Muestra el Checkout de Culqi
+$('#buyButton').on('click', function (e) {
+  // Abre el formulario con las opciones de Culqi.settings
+  Culqi.open();
+  e.preventDefault();
+});
+
+// Envia el token hacia tu servidor
+function culqi() {
+  if (Culqi.token) { // ¡Token creado exitosamente!
+    // Get the token ID:
+    var token = Culqi.token.id;
+    alert('Se ha creado un token:'.token);
+
+  } else { // ¡Hubo algún problema!
+    // Mostramos JSON de objeto error en consola
+    console.log(Culqi.error);
+    alert(Culqi.error.mensaje);
+  }
+}; */
