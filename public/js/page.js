679f699fdbb6b73a8b50dec3f1c2431e12f1eded
kamalyzl <kamaly.kazal@gmail.com>
2018-02-25 17:37:50 -0500

    Agregando subCategoria - nueva peticion a categorias

--- public/js/page.js
+++ public/js/page.js
@@ -1,51 +1,41 @@
+
+// Usamos libreria page.js para generar nuestras rutas dentro de la misma página
 page('/', index);
 page('/categories', categories);
-// page('/user/:userName', user);
-page('*', notfound);
+page('/categories/:list', categories);
+page('/categories/:list/:subLis', categories);
+page('*', function() {
+  $('#contente-search').html('Page not found :(');
+});
 page();
 
 // Llamando elementos principales desde el DOM
 const $btn = $('#btn-search');
 let input = $('#search');
-// let containerSearch = $('#contente-search');
-// let container = document.querySelector('p');
 
+// En la función index, mostramos lo que hará apenas cargue la página
 function index() {
   let containerSearch = $('#contente-search');
   containerSearch.html('<h1>Selecciona en nuestras categorias o en nuestro buscador, el producto deseado</h1>');
   $.get('https://api.mercadolibre.com/sites/MLA/categories', function(data, status) {
     data.forEach(element => {
-      //  console.log(element); 
       let listCategories = $('#categories');
-      listCategories.append(`<a class="dropdown-item" href="/${element.id}" data-id = "${element.id}">${element.name}</a>`);
-
-      // Mostramos todas las categorias y guardamos el id en un atributo data para luego al hacerle click mostrar el contenido
+      listCategories.append(`<a class="dropdown-item" href="/categories/${element.id}" data-id = "${element.id}">${element.name}</a>`);
     });
   });
 }
 
-
-function notfound() {
+// En la función categories agregamos un parametro y lo usamos para ingresar al otro URL de la API
+function categories(ctx) {
   let containerSearch = $('#contente-search');
-  containerSearch.html('<p> cargando...</p>');
-}
-
-
-function user(ctx) {
-  $('p').text('Pagina user' + (ctx.params.userName));
-}
-
-
-$('#categories').on('click', 'a', categories);
-
-function categories() {
-  let containerSearch = $('#contente-search');
-  let id = $(this).data('id');
-  console.log($(this));
-  console.log(id);
-
-  $.get(`https://api.mercadolibre.com/categories/${id}`, function(data, status) {
-    console.log(containerSearch.html(`${data.name}`));
+  containerSearch.html('');
+  $.get(`https://api.mercadolibre.com/categories/${ctx.params.list}`, function(data, status) {
+    data.children_categories.forEach(element => {
+      console.log(`${ctx.params.list}/${element.id}`);
+      containerSearch.append(`
+        <a href="/categories/${ctx.params.list}/${element.id}"  type="button" class="btn btn-dark btn-lg">${element.name}</a>  
+    `);
+    });
   });
 }
 
