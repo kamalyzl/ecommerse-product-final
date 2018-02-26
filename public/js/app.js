39a00dc12d5f509589904efaf8614f80773a5aa8
kamalyzl <kamaly.kazal@gmail.com>
2018-02-26 02:10:08 -0500

    Corrigiendo identación y añadiendo comentarios

--- public/js/app.js
+++ public/js/app.js
@@ -14,11 +14,12 @@ let input = $('#search');
 
 // En la función index, mostramos lo que hará apenas cargue la página
 function index() {
-  let containerSearch = $('#contente-search');
+  $('#contente-list').html('');
+  $('#container-search').html('');
+  $('#contente-search').html(''); 
 
-  containerSearch.html('<h1 class = "text-center"> Bienvenido a Tienda Online </h1>');
+  $('#container-search').html('<h1> Bienvenido a Tienda Online </h1>');
   $.get('https://api.mercadolibre.com/sites/MPE/categories', function(data, status) {
-    // console.log(data);
     data.forEach(element => {     
       let listCategories = $('#categories');
       listCategories.append(`
@@ -32,22 +33,17 @@ function index() {
 function categories(ctx) {
   let containerSearch = $('#contente-search');
   let containerList = $('#contente-list');
-  let name = $('.dropdown-item').data('name');
-  let id = $('#categories').data('id');
-  console.log(ctx.params.categories);
-  // console.log(id);
   
   containerList.html('');
   containerSearch.html('');
+  $('#container-search').html('');
   $.get(`https://api.mercadolibre.com/categories/${ctx.params.categories}`, function(data, status) {   
-    console.log(data);
     containerSearch.append(`      
       <figure>
       <img src="${data.picture}"class="img-fluid" alt="">     
       </figure>
     `);
-    data.children_categories.forEach(element => {    
-      localStorage.setItem('producto', element.name);
+    data.children_categories.forEach(element => {  
       containerSearch.append(`
       <li class="nav-item active">
         <a class="nav-link" href="/${ctx.params.categories}/${element.id}" data-namelist ="${element.name}" id ="nameList">${element.name}<span class="sr-only">(current)</span></a>
@@ -56,51 +52,52 @@ function categories(ctx) {
     });
   });
 }
- 
-// Funcion que lista los productos de cada categoria al momento de seleccionar
-function list(ctx) {
-  let containerList = $('#contente-list');
-  let id = ctx.params.list;
-  let nameList = $('#nameList').data('namelist');
-  
-  console.log(nameList);
-  // containerList.text(localStorage.usuario);
-  // $.get(`https://api.mercadolibre.com/sites/MPE/search?category=${localStorage.usuario}`, function(data, status) {
-     
 
-  //   data.results.forEach(element => {
-  //     containerList.append(` <div class="card m-2" style="width: 18rem;">
-  //     <img class="card-img-top" src="${element.thumbnail}" alt="Card image cap">
-  //     <div class="card-body">
-  //       <h5 class="card-title">S/. ${element.price}</h5>
-  //       <p class="card-text">${element.title}.</p>
-  //       <a href="#" class="btn btn-primary" id="btn-pay">Comprar</a>
-  //     </div>
-  //   </div>`);
-  //   }); 
-  // });
-}
 
 // Evento click para el buscador de productos 
 $btn.click(function(event) {
   event.preventDefault();
-  let containerSearch = $('#contente-search');
-
-  containerSearch.html('');
+  $('#contente-list').html('');
+  $('#contente-search').html('');
+  $('#container-search').html('');
   $.get(`https://api.mercadolibre.com/sites/MPE/search?condition=new&q=${input.val()}`, function(data, status) {
     data.results.forEach(element => {
-      console.log(element);
-      containerSearch.append(` <div class="card m-2" style="width: 18rem;">
+      $('#container-search').append(` <div class="card m-2" style="width: 18rem;">
       <img class="card-img-top" src="${element.thumbnail}" alt="Card image cap">
       <div class="card-body">
-        <h5 class="card-title">S/. ${element.price}</h5>
-        <h6 class="card-title">${element.listing_type_id}</h6>
-        <p class="card-text">${element.title}.</p>
-        <a class="btn btn-primary" id="product" price = "${element.price}" title = "${element.listing_type_id}">Comprar</a>
+      <h5 class="card-title">S/. ${element.price}</h5>
+      <h6 class="card-title">${element.listing_type_id}</h6>
+      <p class="card-text">${element.title}.</p>
+      <a class="btn btn-primary" id="product" price = "${element.price}" title = "${element.listing_type_id}">Comprar</a>
       </div>
-    </div>`);
+      </div>`);
     });
-  });
+    input.val('');
+  });  
 });
 
 
+// Funcion que lista los productos de cada categoria al momento de seleccionar
+function list(ctx) {
+  $('#contente-list').html('');
+  let id = ctx.params.list;
+  let nameList = $('#nameList').data('namelist');
+  console.log($(this));
+  console.log(nameList);
+  console.log(id); 
+
+  /*
+  $.get(`https://api.mercadolibre.com/sites/MPE/search?category=${nameList}`, function(data, status) {
+    data.results.forEach(element => {
+      $('#contente-list').append(` <div class="card m-2" style="width: 18rem;">
+      <img class="card-img-top" src="${element.thumbnail}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">S/. ${element.price}</h5>
+        <p class="card-text">${element.title}.</p>
+        <a href="#" class="btn btn-primary" id="btn-pay">Comprar</a>
+      </div>
+    </div>`);
+    }); 
+  }); 
+  */
+}
