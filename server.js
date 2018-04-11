4170800179edde452f9faf8e06d4e9a88d9ab5cc
kamalyzl <kamaly.kazal@gmail.com>
2018-04-11 11:05:17 -0500

    prueba visión

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); // descargando express
-const app = express(); // guardame todas las funciones de expresss en una variable 'app'
-const server = app.listen(8000, on); // cuando escuches el puerto 3000 recrea la funcion encender
-function on() {
-  console.log('Servidor encendido');
-}
-
-app.use(express.static('public')); // cuando uses express static, la carpeta se llama 'public'
